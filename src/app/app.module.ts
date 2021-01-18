24eb8c6dce04e52793ca6bf698942fef0d8b5451
Sergey Talanov <serg.tlnv@gmail.com>
2021-01-18 18:09:53 +0400

    akita is installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,10 +11,17 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoService } from './services/todo.service';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { TodoFilterComponent } from './components/todo-filter/todo-filter.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, TodoComponent, TodoListComponent, SpinnerComponent, TodoFilterComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot()
+  ],
   providers: [TodoService],
   bootstrap: [AppComponent]
 })
