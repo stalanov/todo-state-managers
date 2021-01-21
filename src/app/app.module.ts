def2a4109b05e090e41b36f12f6cb64cfe4acb8f
Sergey Talanov <serg.tlnv@gmail.com>
2021-01-21 16:58:39 +0400

    ngxs modules are imported into app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,10 +12,18 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoService } from './services/todo.service';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { TodoFilterComponent } from './components/todo-filter/todo-filter.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, TodoComponent, TodoListComponent, SpinnerComponent, TodoFilterComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    NgxsModule.forRoot([], { developmentMode: !environment.production }),
+    NgxsReduxDevtoolsPluginModule.forRoot()
+  ],
   providers: [TodoService],
   bootstrap: [AppComponent]
 })
