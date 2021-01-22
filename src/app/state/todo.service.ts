dc2f11ba44ee84d70cf53707633f57b564b538bf
Sergey Talanov <serg.tlnv@gmail.com>
2021-01-22 19:45:57 +0400

    triggerFlag operator is created

--- src/app/state/todo.service.ts
+++ src/app/state/todo.service.ts
@@ -9,6 +9,11 @@ import { TodoStore } from './todo.store';
 
 type ChangeStateFunction = (state: boolean) => void;
 
+const triggerFlag = (stateFunction: ChangeStateFunction) => <T>(source$: Observable<T>): Observable<T> => {
+  stateFunction(true);
+  return source$.pipe(finalize(() => stateFunction(false)));
+};
+
 @Injectable()
 export class TodoService {
   setLoading: ChangeStateFunction = (state: boolean) => this.todoStore.setLoading(state);
@@ -19,35 +24,36 @@ export class TodoService {
   getTodoList(todoParams: Partial<TodoParams> = {}): Observable<TodoList> {
     const params: Partial<TodoQueryParams> = {};
     Object.entries(todoParams).forEach(([key, value]) => (params[key] = value.toString()));
-    const request = this.http
+    return this.http
       .get<TodoList>(this.getUrl(), { params })
-      .pipe(tap(list => this.todoStore.set(list)));
-    return this.stateWrap(request, this.setLoading);
+      .pipe(
+        triggerFlag(this.setLoading),
+        tap(list => this.todoStore.set(list))
+      );
   }
 
   addTodo(todo: Todo): Observable<Todo> {
-    const request = this.http.post<Todo>(this.getUrl(), todo).pipe(tap(newTodo => this.todoStore.add(newTodo)));
-    return this.stateWrap(request, this.setSaving);
+    return this.http.post<Todo>(this.getUrl(), todo).pipe(
+      triggerFlag(this.setSaving),
+      tap(newTodo => this.todoStore.add(newTodo))
+    );
   }
 
   removeTodo(todo: Todo): Observable<{}> {
-    const request = this.http.delete(this.getUrl(todo.id)).pipe(tap(() => this.todoStore.remove(todo.id)));
-    return this.stateWrap(request, this.setSaving);
+    return this.http.delete(this.getUrl(todo.id)).pipe(
+      triggerFlag(this.setSaving),
+      tap(() => this.todoStore.remove(todo.id))
+    );
   }
 
   updateTodo(todo: Todo): Observable<Todo> {
-    const request = this.http
-      .put<Todo>(this.getUrl(todo.id), todo)
-      .pipe(tap(updatedTodo => this.todoStore.update(updatedTodo.id, updatedTodo)));
-    return this.stateWrap(request, this.setSaving);
+    return this.http.put<Todo>(this.getUrl(todo.id), todo).pipe(
+      triggerFlag(this.setSaving),
+      tap(updatedTodo => this.todoStore.update(updatedTodo.id, updatedTodo))
+    );
   }
 
   private getUrl(id?: number): string {
     return `${environment.apiUrl}/todos` + (id != null ? `/${id}` : '');
-  }
-
-  private stateWrap<T>(request: Observable<T>, stateFunction: (state: boolean) => void): Observable<T> {
-    stateFunction(true);
-    return request.pipe(finalize(() => stateFunction(false)));
   }
 }
